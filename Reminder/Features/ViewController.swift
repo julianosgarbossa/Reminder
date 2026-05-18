8f99132ef3d871a6ceebfbeb19758fbebd02a5ee
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2026-05-18 10:43:42 -0300

    Criação e Configuração Inicial do Projeto

---
+++ Reminder/Features/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Juliano Sgarbossa on 18/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
