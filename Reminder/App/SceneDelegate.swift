49f2f711d98adcef51ca538318529808667faac3
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2026-05-18 11:39:19 -0300

    feat: Criando Tela de Splash e Configurando Launch Screen

--- Reminder/App/SceneDelegate.swift
+++ Reminder/App/SceneDelegate.swift
@@ -15,8 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let viewController = ViewController()
-        let nav = UINavigationController(rootViewController: viewController)
+        let splashViewController = SplashViewController()
+        let nav = UINavigationController(rootViewController: splashViewController)
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
     }
